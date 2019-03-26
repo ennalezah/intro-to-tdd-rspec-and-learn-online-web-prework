6c1d1065755c143bc6d8400a09f6c91acd01ba15
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-03-26 02:06:10 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,2 +1,8 @@
-require relative
-'./current_age_for_birth_year.rb'
+require relative './current_age_for_birth_year.rb'
+
+puts "What year were you born?"
+birth_year = get.to_i
+
+users_age = current_age_for_birth_year(birth_year)
+
+puts "You are: " + usrs_age.to_s + " years old."
